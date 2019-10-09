d2948cd9b3a8d1d4cc88e88f53f79830a5cb1ef0
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-10-08 22:05:04 -0700

    update blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
         <Layout>
             <Head title="Home" />
             <h3>Hi, I'm rsapkf, currently self-learning Computer Science and Full-Stack Web Development.</h3>
-            <p>I mostly work with [<a href="https://github.com/django/django">Python/Django</a>] and [<a href="https://github.com/facebook/react">JavaScript/React</a>]. I also work with the [<a href="#">MERN Stack]</a>, [<a href="https://github.com/pallets/flask">Python/Flask</a>], and a few other CSS and JavaScript Libraries- [<a href="https://github.com/twbs/bootstrap">Bootstrap</a>], [<a href="https://github.com/gatsbyjs/gatsby">GatsbyJS</a>].</p>
+            <p>I mostly work with [<a href="https://github.com/django/django">Python/Django</a>] and [<a href="https://github.com/facebook/react">JavaScript/React</a>]. I also work with the [<a href="https://www.startpage.com/do/metasearch.pl?query=mern%20stack">MERN Stack]</a>, [<a href="https://github.com/pallets/flask">Python/Flask</a>], and a few other CSS and JavaScript Libraries- [<a href="https://github.com/twbs/bootstrap">Bootstrap</a>], [<a href="https://github.com/gatsbyjs/gatsby">GatsbyJS</a>].</p>
             <br></br>
             <p>Please see [<Link to="/technologies">Technologies</Link>] section for more info!</p>
             <hr></hr>
