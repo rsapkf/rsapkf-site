7367773f40c71f82f3ff9dd0a80347bf0da9b5e2
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-10-10 06:42:26 -0700

    Add RSS

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,6 +14,19 @@ module.exports.onCreateNode = ({ node, actions}) => {
     }
 }
 
+// const { createFilePath } = require(`gatsby-source-filesystem`)
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const value = createFilePath({ node, getNode })
+//     createNodeField({
+//       name: `slug`,
+//       node,
+//       value,
+//     })
+//   }
+// }
+
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog.js')
--- src/components/footer.js
+++ src/components/footer.js
@@ -16,6 +16,27 @@ const Footer = () => {
 
     return (
         <footer className={footerStyles.footer}>
+            <div style={{ float: 'right' }}>
+            <a
+            href="https://mastodon.social/@SecretAgentRandyBeans"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            mastodon
+            </a>{' '}
+            &bull;{' '}
+            <a
+            href="https://github.com/rsapkf"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            github
+            </a>{' '}
+            &bull;{' '}
+            <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+                rss
+            </a>
+            </div>
             <p>Copyright © 2019 {data.site.siteMetadata.author}</p>
         </footer>
     )
