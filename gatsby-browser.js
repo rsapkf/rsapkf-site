e1562a3eb535d407b11d63f29031fe91dfffb3da
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-10-22 09:03:03 -0700

    improve prismjs code highlighting

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+// Prismjs Themes
+
+// require("prismjs/themes/prism.css")
+// require("prismjs/themes/prism-dark.css")
+// require("prismjs/themes/prism-funky.css")
+// require("prismjs/themes/prism-okaidia.css")
+// require("prismjs/themes/prism-twilight.css")
+// require("prismjs/themes/prism-coy.css")
+// require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/themes/prism-tomorrow.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
