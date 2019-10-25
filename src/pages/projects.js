38c7ea77ed6d8e9764abb33baf5f3c967f0eb87f
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-10-24 23:43:00 -0700

    update pages

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,7 +9,7 @@ const ProjectsPage = () => {
         <Layout>
             <Head title="Projects"/>
             <h3>Projects</h3>
-            I'm working on a few projects that aren't yet ready to be launched. The projects will be open sourced once they are launched. Please follow via <a href="/rss.xml">RSS</a> to get notified.
+            I'm working on a few projects that aren't yet ready to be launched. All projects will be open sourced upon release. Please follow via <a href="/rss.xml">RSS</a> to get notified.
             <br /><br />
             <p>For other smaller projects that I'm working on, see my <a href="https://github.com/rsapkf">GitHub</a> profile.</p>
         </Layout>
