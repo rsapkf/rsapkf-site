56c661d51f8cd5f9ff6ce56cc55ad79592308492
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-11-02 20:56:19 -0400

    added projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,7 +11,14 @@ const ProjectsPage = () => {
             <h3>Projects</h3>
             I'm working on a few projects that aren't yet ready to be launched. All projects will be open sourced upon release. Please follow via <a href="/rss.xml">RSS</a> to get notified.
             <br /><br />
-            <p>For other smaller projects that I'm working on, see my <a href="https://github.com/rsapkf">GitHub</a> profile.</p>
+            <p>For other smaller projects that I'm working on, see my <a href="https://github.com/rsapkf">GitHub</a> profile:</p>
+            <ul style={{ 
+                fontFamily: 'Yeon Sung',
+                fontSize: '1.0rem',
+             }}>
+                <li>goodies: personal knowledge base <br /><object data="https://img.shields.io/github/stars/rsapkf/goodies?label=Star&amp;style=social"></object></li>
+                <li>dotfiles: my dev environment <br /><object data="https://img.shields.io/github/stars/rsapkf/dotfiles?label=Star&amp;style=social"></object></li>
+            </ul>
         </Layout>
     )
 }
