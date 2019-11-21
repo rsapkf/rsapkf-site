8f4712ffab3242b79515488a882bffafc51ecb22
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-11-21 00:52:15 -0500

    update projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,8 +15,25 @@ const ProjectsPage = () => {
             <ul style={{ 
                 fontFamily: 'Open Sans',
              }}>
-                <li>[<a href="https://github.com/rsapkf/goodies"><b>rsapkf/goodies</b></a>]: Collection of GitHub repos, blogs and websites to learn cool things. <br /><object data="https://img.shields.io/github/stars/rsapkf/goodies?label=Star&amp;style=social"></object></li>
-                <li>[<a href="https://github.com/rsapkf/dotfiles"><b>rsapkf/dotfiles</b></a>]: My Dev Environment. <br /><object data="https://img.shields.io/github/stars/rsapkf/dotfiles?label=Star&amp;style=social"></object></li>
+                <li>
+                    [<a href="https://github.com/rsapkf/goodies"><b>rsapkf/goodies</b></a>]: Collection of GitHub repos, blogs and websites to learn cool things. 
+                    <br />
+                    [<a href="https://goodies.gitbook.io/goodies"><b>GitBook</b></a>]
+                    <br />
+                    <object data="https://img.shields.io/github/stars/rsapkf/goodies?label=Star&amp;style=social"></object>
+                </li>
+                <li>
+                    [<a href="https://github.com/rsapkf/gnureadline-reference"><b>rsapkf/gnureadline-reference</b></a>]: A Comprehensive Reference Sheet for GNU Readline Library shortcuts for VI + Emacs Editing Modes.
+                    <br />
+                    [<a href="https://gnureadline-reference.herokuapp.com/"><b>Website</b></a>]
+                    <br />
+                    <object data="https://img.shields.io/github/stars/rsapkf/gnureadline-reference?label=Star&amp;style=social"></object>
+                </li>
+                <li>
+                    [<a href="https://github.com/rsapkf/dotfiles"><b>rsapkf/dotfiles</b></a>]: My Dev Environment. 
+                    <br />
+                    <object data="https://img.shields.io/github/stars/rsapkf/dotfiles?label=Star&amp;style=social"></object>
+                </li>
             </ul>
         </Layout>
     )
