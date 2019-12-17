83288bb106be48792c10008f8534cee81a54f8ad
rsapkf <PaulAdrienMauriceDirac@protonmail.com>
2019-12-17 09:37:03 -0500

    layout improvements and additions

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,82 +1,101 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import Layout from '../components/layout'
-import Head from '../components/head'
+import React from "react"
+import { navigate } from "gatsby-link"
+import Layout from "../components/layout"
+import Head from "../components/head"
 
-import formStyles from './form.module.scss'
+import formStyles from "./form.module.scss"
 
 function encode(data) {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
 }
 
 export default function Contact() {
   const [state, setState] = React.useState({})
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
     const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
+        "form-name": form.getAttribute("name"),
         ...state,
       }),
     })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
   }
 
   return (
     <Layout>
-      <Head title="Contact Form"/>
-      <u><h3>Contact Form</h3></u>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              NAME
-              <br />
-              <input type="text" name="name" placeholder="Jane" onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              EMAIL
-              <br />
-              <input type="email" name="email" placeholder="jane@pm.me" onChange={handleChange} required/>
-            </label>
-          </p>
-          <p>
-            <label>
-              MESSAGE
-              <br />
-              <textarea name="message" placeholder="Your Message..." onChange={handleChange} required/>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+      <Head title="Contact Form" />
+      <u>
+        <h3>Contact Form</h3>
+      </u>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{" "}
+            <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            NAME
+            <br />
+            <input
+              type="text"
+              name="name"
+              placeholder=""
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            EMAIL
+            <br />
+            <input
+              type="email"
+              name="email"
+              placeholder=""
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            MESSAGE
+            <br />
+            <textarea
+              name="message"
+              placeholder="."
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </Layout>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,42 +1,77 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Head from '../components/head'
+import Head from "../components/head"
 
 const ProjectsPage = () => {
-    return (
-        <Layout>
-            <Head title="Projects"/>
-            <u><h3>Projects</h3></u>
-            I'm working on a few projects that aren't yet ready to be launched. All projects will be open sourced upon release. Please follow via <a href="/rss.xml">RSS</a> to get notified.
-            <br /><br />
-            <p>For other smaller projects that I'm working on, see my <a href="https://github.com/rsapkf">GitHub</a> profile:</p>
-            <ul style={{ 
-                fontFamily: 'Open Sans',
-             }}>
-                <li>
-                    [<a href="https://github.com/rsapkf/goodies"><b>rsapkf/goodies</b></a>]: Collection of GitHub repos, blogs and websites to learn cool things. 
-                    <br />
-                    [<a href="https://goodies.gitbook.io/goodies"><b>GitBook</b></a>]
-                    <br />
-                    <object data="https://img.shields.io/github/stars/rsapkf/goodies?label=Star&amp;style=social"></object>
-                </li>
-                <li>
-                    [<a href="https://github.com/rsapkf/gnureadline-reference"><b>rsapkf/gnureadline-reference</b></a>]: A Comprehensive Reference Sheet for GNU Readline Library shortcuts for VI + Emacs Editing Modes.
-                    <br />
-                    [<a href="https://gnureadline-reference.herokuapp.com/"><b>Website</b></a>]
-                    <br />
-                    <object data="https://img.shields.io/github/stars/rsapkf/gnureadline-reference?label=Star&amp;style=social"></object>
-                </li>
-                <li>
-                    [<a href="https://github.com/rsapkf/dotfiles"><b>rsapkf/dotfiles</b></a>]: My Dev Environment. 
-                    <br />
-                    <object data="https://img.shields.io/github/stars/rsapkf/dotfiles?label=Star&amp;style=social"></object>
-                </li>
-            </ul>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head title="Projects" />
+      <u>
+        <h3>Projects</h3>
+      </u>
+      I'm working on a few projects that aren't yet ready to be launched. All
+      projects will be open sourced upon release. Please follow via{" "}
+      <a href="/rss.xml">RSS</a> to get notified.
+      <br />
+      <br />
+      <p>
+        For other smaller projects that I'm working on, see my{" "}
+        <a href="https://github.com/rsapkf">GitHub</a> profile:
+      </p>
+      <ul
+        style={{
+          fontFamily: "Open Sans",
+        }}
+      >
+        <li>
+          [
+          <a href="https://github.com/rsapkf/goodies">
+            <b>rsapkf/goodies</b>
+          </a>
+          ]: Collection of GitHub repos, blogs and websites to learn cool
+          things.
+          <br />[
+          <a href="https://rsapkf.github.io/goodies">
+            <b>Official Website</b>
+          </a>
+          (built with Rust and mdBook)]
+          <br /> [
+          <a href="https://goodies.gitbook.io/goodies">
+            <b>GitBook</b>
+          </a>
+          ]
+          <br />
+          <object data="https://img.shields.io/github/stars/rsapkf/goodies?label=Star&amp;style=social"></object>
+        </li>
+        <li>
+          [
+          <a href="https://github.com/rsapkf/gnureadline-reference">
+            <b>rsapkf/gnureadline-reference</b>
+          </a>
+          ]: A Comprehensive Reference Sheet for GNU Readline Library shortcuts
+          for VI + Emacs Editing Modes.
+          <br />[
+          <a href="https://gnureadline-reference.herokuapp.com/">
+            <b>Website</b>
+          </a>
+          ]
+          <br />
+          <object data="https://img.shields.io/github/stars/rsapkf/gnureadline-reference?label=Star&amp;style=social"></object>
+        </li>
+        <li>
+          [
+          <a href="https://github.com/rsapkf/dotfiles">
+            <b>rsapkf/dotfiles</b>
+          </a>
+          ]: My Dev Environment.
+          <br />
+          <object data="https://img.shields.io/github/stars/rsapkf/dotfiles?label=Star&amp;style=social"></object>
+        </li>
+      </ul>
+    </Layout>
+  )
 }
 
 export default ProjectsPage
