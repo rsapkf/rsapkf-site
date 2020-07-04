0d2e450123c79121a3c82813de509bfbb9fe9890
rsapkf <rsapkfff@pm.me>
2020-07-03 22:54:54 -0400

    fix typo in homework, update components

--- src/components/BlogNav.js
+++ src/components/BlogNav.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby'
-import BlogNavStyles from './BlogNavStyles.module.scss'
+import blogNavStyles from './blognav.module.scss'
 
 export default function BlogNav({ prev, next }) {
     console.log(prev)
     return (
-        <div className={BlogNavStyles.container}>
+        <div className={blogNavStyles.container}>
             {prev && (
                 <div>
                     <Link to={`/blog/${prev.node.fields.slug}`}>
-                        <span className={BlogNavStyles.prev} >← Previous</span><br />
+                        <span className={blogNavStyles.prev} >← Previous</span><br />
                     </Link>
-                    <span className={BlogNavStyles.title}>{prev.node.frontmatter.title}</span>
+                    <span className={blogNavStyles.title}>{prev.node.frontmatter.title}</span>
                 </div>
             )}
             {
                 next && (
                     <div>
                         <Link to={`/blog/${next.node.fields.slug}`}>
-                            <span className={BlogNavStyles.next}>Next →</span><br />
+                            <span className={blogNavStyles.next}>Next →</span><br />
                         </Link>
-                        <span className={BlogNavStyles.title}>{next.node.frontmatter.title}</span>
+                        <span className={blogNavStyles.title}>{next.node.frontmatter.title}</span>
                     </div>
                 )
             }
