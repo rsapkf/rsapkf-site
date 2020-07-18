895cf94282ebb88aa8bc1360619c78c34ef958be
rsapkf <rsapkfff@pm.me>
2020-07-18 01:11:56 -0400

    fix Spinner svg styles on theme change

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import spinner from '../img/grid.svg'
+import React from "react"
+import spinner from "../img/grid.svg"
+
+import ThemeContext from "../context/ThemeContext"
 
 const Spinner = () => {
-    return (
-        <>
-            <img
-                src={spinner}
-                style={{ width: '35px', margin: 'auto', display: 'block' }}
-                alt='Loading'
-            />
-        </>
-    )
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <div>
+          <img
+            className
+            src={spinner}
+            style={{
+              width: "35px",
+              margin: "auto",
+              display: "block",
+              filter: !theme.dark ? "invert(1)" : null,
+            }}
+            alt="Loading..."
+          />
+        </div>
+      )}
+    </ThemeContext.Consumer>
+  )
 }
 
 export default Spinner
